33ff60339fac6d4ff21e4e29ca79f614f0831c11
VICTORIA FIEDOTIN <86FI40903400@campus.economicas.uba.ar>
2022-03-30 21:50:48 -0300

    Ejercicio real

--- Guia1_Ejercicio1/Program.cs
+++ Guia1_Ejercicio1/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(“Buen día”);
+﻿//Ejercicio 1
+Console.WriteLine("Hola Mundo");
+Console.WriteLine("Presione cualquier tecla para salir");
+
